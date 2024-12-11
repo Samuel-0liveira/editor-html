0b70c5279a9e17a8b3c10511b10e9518af1b7d81
Samuel Oliveira <S.SamuelS.a.m@hotmail.com>
2024-12-11 15:59:24 -0300

    Criação do método "EscreverOpcoes" e acréscimo de linhas da tela.

--- Menu.cs
+++ Menu.cs
@@ -12,13 +12,16 @@ namespace EditorHtml
             Console.ForegroundColor = ConsoleColor.Black;
 
             DesenharTela();
+            EscreverOpcoes();
+
+            Console.ReadKey();
         }
 
         public static void DesenharTela()
         {
             CabecalhoRodape();
 
-            for (int linhas = 0; linhas <= 10; linhas++)
+            for (int linhas = 0; linhas <= 15; linhas++)
             {
                 Console.Write("|");
                 for (int espaco = 0; espaco <= 35; espaco++)
@@ -41,6 +44,29 @@ namespace EditorHtml
             }
             Console.Write("+");
             Console.Write("\n");
+        }
+
+        public static void EscreverOpcoes()
+        {
+            Console.SetCursorPosition(12,1);
+            Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(1,2);
+            Console.WriteLine("------------------------------------");
+
+            Console.SetCursorPosition(3,3);
+            Console.WriteLine("Selecione uma opção abaixo: ");
+
+            Console.SetCursorPosition(3,5);
+            Console.WriteLine("1 - Novo arquivo");
+
+            Console.SetCursorPosition(3,7);
+            Console.WriteLine("2 - Abrir arquivo");
+
+            Console.SetCursorPosition(3,9);
+            Console.WriteLine("0 - Sair");
+
+            Console.SetCursorPosition(3,12);
+            Console.Write("Opção: ");
         }
     }
 }
